e121c1d1d9a71d61947ce2f4a4005cdc3887eb99
elgroselj <ema.ljubljana@gmail.com>
2024-05-14 14:09:19 +0200

    Update construct_db, gitignore pickles

--- construct_db.py
+++ construct_db.py
@@ -6,26 +6,32 @@ from helper_functions import read_graph_file, path_to_graph_name
 
 db_name = "graphs.csv"
 
+try:
+    df = pd.read_csv(db_name)
+    print(f"INFO: Openning an existing DB '{db_name}'.")
+except FileNotFoundError:
+    df = pd.DataFrame(columns=["graph_name","source","pickle_path","opt"])
+    print(f"INFO: A new DB {db_name} was created.")
 
-# df = pd.DataFrame(columns=["graph_name","source","pickle_path","opt"])
-df = pd.read_csv(db_name)
-
-
-for path_obj in Path('data').rglob('*.al'):
+paths = list(Path('data').rglob('*.al'))
+paths += list(Path('data').rglob('*.d'))
+for path_obj in paths:
     path = str(path_obj)
     graph_name = path_to_graph_name(path)
-    # if graph_name not in df["graph_name"]:
-    if True:
+    
+    if graph_name not in list(df["graph_name"]):
         print(f"INFO: Reading {graph_name}")
-        G, graph_name2, source, their_result = read_graph_file(path)
-        assert graph_name == graph_name2
+        G, _, source, their_result = read_graph_file(path)
         pickle_path = f"pickles/{graph_name}.pickle"
+        
         with open(pickle_path, "wb") as f:
             pickle.dump(G, f)
+            
         df.loc[len(df)] = [graph_name,source,pickle_path,their_result]
     else:
         print(f"INFO: Graph {graph_name} already in DB.")
 
-df.to_csv(db_name)
+df.to_csv(db_name, index=False)
+print(f"INFO: A DB {db_name} was saved.")
 
     
