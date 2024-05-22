691e55578df9325fe43a81450d872718243fdaa1
elgroselj <ema.ljubljana@gmail.com>
2024-05-22 11:17:20 +0200

    Create small csv, move chars, compute chars, visualize chars

--- playground/compute_chars.py
+++ playground/compute_chars.py
@@ -4,9 +4,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 # import cugraph as nx
-from networkx.algorithms import apx
+# from networkx.algorithms import apx
 
-db_name = "components.csv"
+# db_name = "components.csv"
+db_name = "components_small.csv"
 df = pd.read_csv(db_name)
 
 
@@ -25,8 +26,9 @@ def elementary_measures(l,name):
 cols = list(df.columns)
 new_cols = None
 for i in range(len(df)):
-    if i % 1000 == 0:
-        print(f"INFO: Working on {i}th row ...")
+    # if i % 1000 == 0:
+    #     print(f"INFO: Working on {i}th row ...")
+    print(f"INFO: Working on {i}th row ...")
     
     row = df.iloc[i]    
     pickle_path = row["pickle_path"]
@@ -38,12 +40,14 @@ for i in range(len(df)):
     
     if new_cols is None or "n" in new_cols:
         d["n"] = G.number_of_nodes()
+        if d["n"] == 1:
+            continue
     if new_cols is None or "m" in new_cols:
         d["m"] = G.number_of_edges()
     
-    '''if new_cols is None or "parallel_edges" in new_cols:
-        A= nx.adjacency_matrix(G)
-        d["parallel_edges"] = len(np.where(A+A.T == 2))/2
+    # if new_cols is None or "parallel_edges" in new_cols:
+    #     A= nx.adjacency_matrix(G)
+    #     d["parallel_edges"] = len(np.where(A+A.T == 2))/2
     
     
     if new_cols is None or "density" in new_cols:
@@ -69,7 +73,7 @@ for i in range(len(df)):
     #     d["diameter_LB"] = apx.diameter(G)
         
     if new_cols is None or "node_connectivity" in new_cols:
-        d["node_connectivity"] = nx.node_connectivity()
+        d["node_connectivity"] = nx.node_connectivity(G)
         # minimum number of nodes that must be removed to disconnect G
     
     # if new_cols is None or "node_connectivity_LB" in new_cols:
@@ -78,7 +82,33 @@ for i in range(len(df)):
     
     if new_cols is None or "degree_min" in new_cols:
         degrees = np.array([deg for n,deg in G.degree])
-        d.update(elementary_measures(degrees,"degree"))'''
+        d.update(elementary_measures(degrees,"degree"))
+        
+    if new_cols is None or "closeness_centrality" in new_cols:
+        d["closeness_centrality"] = nx.closeness_centrality(G)
+        
+    if new_cols is None or "betweenness_centrality" in new_cols:
+        d["betweenness_centrality"] = nx.betweenness_centrality(G)
+        
+        
+    if new_cols is None or "degree_centrality" in new_cols:
+        d["degree_centrality"] = nx.degree_centrality(G)
+        
+    # if new_cols is None or "eigenvector_centrality" in new_cols:
+    #     d["eigenvector_centrality"] = nx.eigenvector_centrality(G)
+        
+    if new_cols is None or "clustering" in new_cols:
+        d["clustering"] = nx.clustering(G)
+    if new_cols is None or "transitivity" in new_cols:
+        d["transitivity"] = nx.transitivity(G)
+    if new_cols is None or "katz_centrality" in new_cols:
+        d["katz_centrality"] = nx.katz_centrality(G)
+    if new_cols is None or "pagerank" in new_cols:
+        d["pagerank"] = nx.pagerank(G)
+        
+  
+        
+    
         
     
     
@@ -110,10 +140,13 @@ for i in range(len(df)):
             if key not in df.columns:
                 df[key] = len(df) * [None]
                 new_cols.append(key)
-            df.iloc[i][key] = d[key]
+            # df.iloc[i][key] = d[key]
+            df.at[i,key] = d[key]
     else:
         for col in new_cols:
-            df.iloc[i][col] = d[col]
+            # df.iloc[i][col] = d[col]
+            df.at[i,col] = d[col]
     
 
-df.to_csv("chars2_" + db_name, index=False)
+# df.to_csv("chars_" + db_name, index=False)
+df.to_csv("chars_" + db_name, index=False)
