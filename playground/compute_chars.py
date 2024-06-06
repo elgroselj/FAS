964c5e7ac5c36839384add80e96592d80bfba5bd
elgroselj <ema.ljubljana@gmail.com>
2024-06-06 21:05:14 +0200

    Compute all chars

--- playground/compute_chars.py
+++ playground/compute_chars.py
@@ -6,18 +6,44 @@ import numpy as np
 # import cugraph as nx
 from networkx.algorithms import approximation as apx
 
+import sys
+sys.path.append(".")
+from timeout import timeout
+from time import sleep
+
+
 
 import sys
 sys.path.append(".")
 from solvers.lower_bound import LB
 from helper_functions import repeat_max 
 
-# db_name = "components.csv"
-db_name = "components_small.csv"
+db_name = "3_2_1_components.csv"
+# db_name = "components_small.csv"
 df = pd.read_csv(db_name)
 
+@timeout(1*60)
+def to(lam):
+    return lam()
+
+def call(lam):
+    try:
+        res = to(lam)
+        return res
+    except Exception:
+        print(f"WARNING: Timedout: {lam}")
+        return None
+        
+    
 
 def elementary_measures(l,name):
+    if l is None:
+        return {f"{name}_min":None,
+         f"{name}_max":None,
+         f"{name}_p25":None,
+         f"{name}_p50":None,
+         f"{name}_p75":None,
+         }
     p25, p50, p75 = np.percentile(l,[25, 50, 75])
     maxi = np.max(l)
     mini = np.min(l)
@@ -29,8 +55,18 @@ def elementary_measures(l,name):
          }
     return d
 
+
+        
+    
+    
+
+
 cols = list(df.columns)
 new_cols = None
+
+
+
+MODE = 4
 for i in range(len(df)):
     # if i % 1000 == 0:
     #     print(f"INFO: Working on {i}th row ...")
@@ -41,144 +77,108 @@ for i in range(len(df)):
     with open(pickle_path, "rb") as f:
         G = pickle.load(f)
         assert nx.is_directed(G)
+        
+    if G.number_of_nodes() <= 1:
+        continue
     
     d = {}
     
-    if new_cols is None or "n" in new_cols:
-        d["n"] = G.number_of_nodes()
-        if d["n"] == 1:
-            continue
-    if new_cols is None or "m" in new_cols:
-        d["m"] = G.number_of_edges()
-    
-    # if new_cols is None or "parallel_edges" in new_cols:
-    #     A= nx.adjacency_matrix(G)
-    #     d["parallel_edges"] = len(np.where(A+A.T == 2))/2
-    
-    
-    if new_cols is None or "density" in new_cols:
-        d["density"] = nx.density(G)
-    
-    if new_cols is None or "planar" in new_cols:
-        d["planar"] = nx.is_planar(G)
-    
-    if new_cols is None or "bipartite" in new_cols:
-        d["bipartite"] = nx.is_bipartite(G)
+    # 1
+    if MODE == 1:
+        if new_cols is None or "n" in new_cols:
+            d["n"] = call(G.number_of_nodes)
+            
+        if new_cols is None or "m" in new_cols:
+            d["m"] = call(G.number_of_edges)
         
-    # if new_cols is None or "radius" in new_cols:
-    #     ecc = nx.eccentricity(G)
-    #     d["radius"] = np.min(ecc) #nx.radius(G)
-    #     d["diameter"] = np.max(ecc) #nx.diameter(G)
+
+        if new_cols is None or "density" in new_cols:
+            d["density"] = call(lambda : nx.density(G))
         
-    if new_cols is None or "radius" in new_cols:
-        d["radius"] = nx.radius(G)
-    if new_cols is None or "diameter" in new_cols:
-        d["diameter"] = nx.diameter(G)
-    
-    # if new_cols is None or "diameter_LB" in new_cols:
-    #     d["diameter_LB"] = apx.diameter(G)
+        if new_cols is None or "planar" in new_cols:
+            d["planar"] = call(lambda : nx.is_planar(G))
         
-    if new_cols is None or "node_connectivity" in new_cols:
-        d["node_connectivity"] = nx.node_connectivity(G)
-        # minimum number of nodes that must be removed to disconnect G
+        if new_cols is None or "bipartite" in new_cols:
+            d["bipartite"] = call(lambda : nx.is_bipartite(G))
+            
+            
+        if new_cols is None or "radius" in new_cols:
+            d["radius"] = call(lambda : nx.radius(G))
+        if new_cols is None or "diameter" in new_cols:
+            d["diameter"] = call(lambda : nx.diameter(G))
     
-    # if new_cols is None or "node_connectivity_LB" in new_cols:
-    #     d["node_connectivity_LB"] = apx.node_connectivity()
-    #     # LB for minimum number of nodes that must be removed to disconnect G
-    
-    if new_cols is None or "degree_min" in new_cols:
-        degrees = np.array([deg for n,deg in G.degree])
-        d.update(elementary_measures(degrees,"degree"))
+
+
+    # 2
+    if MODE == 2:
+        if new_cols is None or "node_connectivity" in new_cols:
+            d["node_connectivity"] = call(lambda : nx.node_connectivity(G))
+            # minimum number of nodes that must be removed to disconnect G
         
-    if new_cols is None or "closeness_centrality_min" in new_cols:
-        vals = np.array([val for n,val in nx.closeness_centrality(G).items()])
-        d.update(elementary_measures(vals,"closeness_centrality"))
+        if new_cols is None or "transitivity" in new_cols:
+            d["transitivity"] = call(lambda : nx.transitivity(G))
+            
+        # cast v NEUSMERJEN graf
+        if new_cols is None or "treewidth_min_fill_in" in new_cols:
+            res = call(lambda : apx.treewidth_min_fill_in(nx.Graph(G)))
+            d["treewidth_min_fill_in"] = None if res is None else res[0]
+            
+        if new_cols is None or "treewidth_min_degree" in new_cols:
+            res = call(lambda : apx.treewidth_min_degree(nx.Graph(G)))
+            d["treewidth_min_degree"] = None if res is None else res[0]
+            
         
         
-    if new_cols is None or "betweenness_centrality_min" in new_cols:
-        vals = np.array([val for n,val in nx.betweenness_centrality(G).items()])
-        d.update(elementary_measures(vals,"betweenness_centrality"))
         
-    # if new_cols is None or "betweenness_centrality" in new_cols:
-    #     d["betweenness_centrality"] = nx.betweenness_centrality(G)
-    
-    
-    if new_cols is None or "degree_centrality_min" in new_cols:
-        vals = np.array([val for n,val in nx.degree_centrality(G).items()])
-        d.update(elementary_measures(vals,"degree_centrality"))
         
-    # if new_cols is None or "degree_centrality" in new_cols:
-    #     d["degree_centrality"] = nx.degree_centrality(G)
-        
-    # ne dela na vseh, izpuscam
-    # if new_cols is None or "eigenvector_centrality" in new_cols:
-        #     d["eigenvector_centrality"] = nx.eigenvector_centrality(G)
-    
-    
-    if new_cols is None or "clustering_min" in new_cols:
-        vals = np.array([val for n,val in nx.clustering(G).items()])
-        d.update(elementary_measures(vals,"clustering"))
-    # if new_cols is None or "clustering" in new_cols:
-    #     d["clustering"] = nx.clustering(G)
-        
+    # 3
+    if MODE == 3:
+
+        if new_cols is None or "degree_min" in new_cols:
+            degrees = call(lambda : np.array([deg for n,deg in G.degree]))
+            d.update(elementary_measures(degrees,"degree"))
+            
+        if new_cols is None or "closeness_centrality_min" in new_cols:
+            vals = call(lambda : np.array([val for n,val in nx.closeness_centrality(G).items()]))
+            d.update(elementary_measures(vals,"closeness_centrality"))
+            
+            
+        if new_cols is None or "betweenness_centrality_min" in new_cols:
+            vals = call(lambda : np.array([val for n,val in nx.betweenness_centrality(G).items()]))
+            d.update(elementary_measures(vals,"betweenness_centrality"))
+            
     
         
-    if new_cols is None or "transitivity" in new_cols:
-        d["transitivity"] = nx.transitivity(G)
+        if new_cols is None or "degree_centrality_min" in new_cols:
+            vals = call(lambda : np.array([val for n,val in nx.degree_centrality(G).items()]))
+            d.update(elementary_measures(vals,"degree_centrality"))
+            
+
         
-    if new_cols is None or "katz_centrality_min" in new_cols:
-        vals = np.array([val for n,val in nx.katz_centrality(G).items()])
-        d.update(elementary_measures(vals,"katz_centrality"))
+        if new_cols is None or "clustering_min" in new_cols:
+            vals = call(lambda : np.array([val for n,val in nx.clustering(G).items()]))
+            d.update(elementary_measures(vals,"clustering"))
+            
+
+            
+        if new_cols is None or "katz_centrality_min" in new_cols:
+            vals = call(lambda : np.array([val for n,val in nx.katz_centrality(G).items()]))
+            d.update(elementary_measures(vals,"katz_centrality"))
         
-    # if new_cols is None or "katz_centrality" in new_cols:
-    #     d["katz_centrality"] = nx.katz_centrality(G)
-    
-    
-    if new_cols is None or "pagerank_min" in new_cols:
-        vals = np.array([val for n,val in nx.pagerank(G).items()])
-        d.update(elementary_measures(vals,"pagerank"))
-    # if new_cols is None or "pagerank" in new_cols:
-    #     d["pagerank"] = nx.pagerank(G)
-    
-    
-    
-    
-    
-    # cast v NEUSMERJEN graf
-    if new_cols is None or "treewidth_min_fill_in" in new_cols:
-        d["treewidth_min_fill_in"], _ = apx.treewidth_min_fill_in(nx.Graph(G))
         
-    if new_cols is None or "treewidth_min_degree" in new_cols:
-        d["treewidth_min_degree"], _ = apx.treewidth_min_degree(nx.Graph(G))
+        if new_cols is None or "pagerank_min" in new_cols:
+            vals = call(lambda : np.array([val for n,val in nx.pagerank(G).items()]))
+            d.update(elementary_measures(vals,"pagerank"))
+
         
-  
+
         
-    if new_cols is None or "LB" in new_cols:
-        d["LB"] = repeat_max(lambda: LB(G), 5)
+    # 4
+    if MODE == 4:
+        if new_cols is None or "LB" in new_cols:
+            d["LB"] = repeat_max(lambda: LB(G), 10)
     
     
-        
-    
-    
-   
-    # cycle_lengths = [len(c) for c in nx.simple_cycles(G)]
-    # d.update(elementary_measures(cycle_lengths, "cycle_length"))
-    
-    # NAJVECJA SCC
-    # ccs = list(nx.strongly_connected_components(G))
-    # ccs_lengths = list(map(lambda x: len(x), ccs))
-    # ind = np.argmax(ccs_lengths)
-    # mcc_nodes = ccs[ind] # najvec vozlisc maximum connected component
-    # mcc = nx.subgraph(G,mcc_nodes)
-    
-    
-    # degrees = np.array([deg for n,deg in mcc.degree])
-    # d.update(elementary_measures(degrees, "mcc_degree"))
-    # # cycle_lengths = [len(c) for c in nx.simple_cycles(mcc)]
-    # # d.update(elementary_measures(cycle_lengths, "mcc_cycle_length"))
-    
-    
-    # TODO https://www.geeksforgeeks.org/number-of-triangles-in-directed-and-undirected-graphs/
     
     # se pozene le prvic
     if new_cols is None: 
@@ -197,4 +197,4 @@ for i in range(len(df)):
     
 
 # df.to_csv("chars_" + db_name, index=False)
-df.to_csv("chars_" + db_name, index=False)
+df.to_csv(f"{MODE}_" + db_name, index=False)
